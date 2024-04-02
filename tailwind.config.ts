e96113eea2d135c569d3cd868816e8baaaa7e5f9
Matheus <matheusfariasdasneves@gmail.com>
2024-04-02 20:53:45 -0300

    chore: add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#590BD8",
+        primaryLighter: "#DDD5EA",
+        primaryDarker: "#312A4F",
+        primaryGray: "#717171",
+        grayLighter: "#BBBFBF",
+      },
+      textColor: {
+        dark: "#717171",
+      },
     },
   },
   plugins: [],
