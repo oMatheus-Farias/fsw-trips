84ba9948260f967748a278287bfd4a8cfe223ca8
Matheus <matheusfariasdasneves@gmail.com>
2024-04-11 01:12:13 -0300

    fix: add suspense to trips page

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -4,6 +4,7 @@ import TripItem from "@/components/trip-item";
 import { Trip } from "@prisma/client";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { Suspense } from "react";
 
 const TripsPage = () => {
   const [trips, setTrips] = useState<Trip[]>([]);
@@ -25,23 +26,25 @@ const TripsPage = () => {
   }, []);
 
   return (
-    <div className="container mx-auto flex flex-col p-5 items-center">
-      <h1 className="text-primaryDarker font-semibold text-xl">
-        Viagens Encontradas
-      </h1>
+    <Suspense>
+      <div className="container mx-auto flex flex-col p-5 items-center">
+        <h1 className="text-primaryDarker font-semibold text-xl">
+          Viagens Encontradas
+        </h1>
 
-      <h2 className="text-primaryGray font-medium mb-5">
-        {trips.length > 0
-          ? "Listamos as melhores viagens para você!"
-          : "Não encontramos nada nos seus parâmetros de busca."}
-      </h2>
+        <h2 className="text-primaryGray font-medium mb-5">
+          {trips.length > 0
+            ? "Listamos as melhores viagens para você!"
+            : "Não encontramos nada nos seus parâmetros de busca."}
+        </h2>
 
-      <div className="flex flex-col gap-4">
-        {trips.map((trip) => (
-          <TripItem key={trip.id} trip={trip} />
-        ))}
+        <div className="flex flex-col gap-4">
+          {trips.map((trip) => (
+            <TripItem key={trip.id} trip={trip} />
+          ))}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
