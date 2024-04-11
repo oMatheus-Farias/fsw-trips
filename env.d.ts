8647c3dcf609e7e1e258526409cdf3f48876360a
Matheus <matheusfariasdasneves@gmail.com>
2024-04-11 15:46:11 -0300

    feat: add environment variable host url

--- env.d.ts
+++ env.d.ts
@@ -4,5 +4,9 @@ declare namespace NodeJS {
     GOOGLE_CLIENT_ID: string;
     GOOGLE_CLIENT_SECRET: string;
     NEXTAUTH_SECRET: string;
+    HOST_URL: string;
+    NEXT_PUBLIC_STRIPE_KEY: string;
+    STRIPE_SECRET_KEY: string;
+    STRIPE_WEBHOOK_SECRET_KEY: string;
   }
 }
